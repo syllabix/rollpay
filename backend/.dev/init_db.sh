0a1577d6860bd6c4ef27944ff0693639c7595b6c
tstoepker <tstoepker@plaid.com>
2021-05-14 17:25:37 +0200

    chore: fix db init scripts and ascii art

--- backend/.dev/init_db.sh
+++ backend/.dev/init_db.sh
@@ -3,5 +3,5 @@ set -e
 
 for d in ./databases/*/ ; do
     d=${d:12:${#d}-13}
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --command="create database ${d}"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --command="create database ${d}"
 done
